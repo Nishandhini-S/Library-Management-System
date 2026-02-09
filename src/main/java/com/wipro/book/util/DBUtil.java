94a4b0335648d4f32c305e87c4f19dc31036d416
Nishandhini-S <717823p236@kce.ac.in>
2026-02-09 11:34:31 +0530

    Initial commit

---
+++ src/main/java/com/wipro/book/util/DBUtil.java
@@ -0,0 +1,26 @@
+package com.wipro.book.util;
+
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.SQLException;
+
+public class DBUtil {
+	    public static Connection getDBConnection() {
+	        Connection connection = null;
+	        try {  
+	            Class.forName("oracle.jdbc.driver.OracleDriver");
+	            String url = "jdbc:oracle:thin:@localhost:1521:XE";
+	            String username = "nisha";
+	            String password = "Nisha2005";
+
+	            connection = DriverManager.getConnection(url, username, password);
+
+	        } catch (ClassNotFoundException | SQLException e) {
+	            System.out.println("Database connection error");
+	            e.printStackTrace();
+	        }
+
+	        return connection;
+	    }
+	}
+
